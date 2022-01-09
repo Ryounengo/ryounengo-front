bb47d26a79c4683378c42a90bd67de99d79b8f50
Therence BRUNE <therence.brune@itsfactory.fr>
2022-01-09 17:17:57 +0100

    moved modal to list

--- src/screens/Card/CardList/CardList/CardDetails.tsx
+++ src/screens/Card/CardList/CardList/CardDetails.tsx
@@ -1,38 +1,20 @@
-import { Modal, Pressable, useDisclose } from "native-base";
+import { Pressable } from "native-base";
 import { CardSummary } from "@common/Card/CardSummary/CardSummary";
-import { ICardSummary } from "@typings/interfaces";
+import { ICard, ICardSummary } from "@typings/interfaces";
 import { useStyle } from "@screens/Card/CardList/CardList/styles";
-import { CardFront } from "@screens/Card/CardList/CardList/CardDetails/CardFront";
-import { Animated } from "react-native";
-import { CardBack } from "@screens/Card/CardList/CardList/CardDetails/CardBack";
-import { useFlipAnimation } from "@hooks/Card/useFlipAnimation";
 
 interface IParams {
     card: ICardSummary;
+    setCardDetails(card?: ICard): void;
 }
 
 export const CardDetails = (props: IParams) => {
-    const { card } = props;
+    const { card, setCardDetails } = props;
     const style = useStyle();
-    const { isOpen, onClose, onOpen } = useDisclose();
-    const { flipToFrontStyle, flipToBackStyle, flipCard } = useFlipAnimation();
-    const displayOptions = () => console.log("options");
 
     return (
-        <>
-            <Pressable style={style.card} onLongPress={displayOptions} onPress={onOpen}>
-                <CardSummary card={card} />
-            </Pressable>
-            <Modal isOpen={isOpen} size="md" onClose={onClose}>
-                <Pressable style={style.cardDetail} onPress={flipCard} onTouchMove={onClose}>
-                    <Animated.View style={[style.cardFront, flipToFrontStyle]}>
-                        <CardFront card={card} />
-                    </Animated.View>
-                    <Animated.View style={[style.cardBack, flipToBackStyle]}>
-                        <CardBack card={card} />
-                    </Animated.View>
-                </Pressable>
-            </Modal>
-        </>
+        <Pressable style={style.card} onPress={() => setCardDetails(card)}>
+            <CardSummary card={card} />
+        </Pressable>
     );
 };
--- src/screens/Card/CardList/CardList/CardList.tsx
+++ src/screens/Card/CardList/CardList/CardList.tsx
@@ -1,8 +1,13 @@
-import { View } from "native-base";
-import { ICardSummary } from "@typings/interfaces";
+import { Modal, Pressable, View } from "native-base";
+import { ICard, ICardSummary } from "@typings/interfaces";
 import { useStyle } from "@screens/Card/CardList/CardList/styles";
 import { NoResult } from "@common/NoResult/NoResult";
 import { CardDetails } from "@screens/Card/CardList/CardList/CardDetails";
+import { Animated } from "react-native";
+import { CardFront } from "@screens/Card/CardList/CardList/CardDetails/CardFront";
+import { CardBack } from "@screens/Card/CardList/CardList/CardDetails/CardBack";
+import { useState } from "react";
+import { useFlipAnimation } from "@hooks/Card/useFlipAnimation";
 
 interface IParams {
     cardList?: ICardSummary[];
@@ -11,15 +16,33 @@ interface IParams {
 export const CardList = (props: IParams) => {
     const { cardList } = props;
     const style = useStyle();
+    const [cardDetails, setCardDetails] = useState<ICard>();
+    const { flipToFrontStyle, flipToBackStyle, flipCard } = useFlipAnimation(cardDetails);
 
     return (
         <View style={style.container}>
             <View style={style.cardList}>
                 {cardList?.map((card) => (
-                    <CardDetails card={card} key={card.id} />
+                    <CardDetails card={card} key={card.id} setCardDetails={setCardDetails} />
                 ))}
             </View>
             {cardList?.length === 0 && <NoResult />}
+            <Modal isOpen={Boolean(cardDetails)} onClose={() => setCardDetails(undefined)}>
+                {cardDetails && (
+                    <Pressable
+                        style={style.cardDetail}
+                        onPress={flipCard}
+                        onTouchMove={() => setCardDetails(undefined)}
+                    >
+                        <Animated.View style={[style.cardFront, flipToFrontStyle]}>
+                            <CardFront card={cardDetails} />
+                        </Animated.View>
+                        <Animated.View style={[style.cardBack, flipToBackStyle]}>
+                            <CardBack card={cardDetails} />
+                        </Animated.View>
+                    </Pressable>
+                )}
+            </Modal>
         </View>
     );
 };
